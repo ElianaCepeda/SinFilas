f9606392f67a034bd5e91d489e5e5ab89383f109
HECTOR EDUARDO ASTUDILLO <calvo2244@hotmail.com>
2021-11-07 19:29:09 -0500

    actualizacion activity
    se crean botones y conexiones entre las actividades bienvenido y login

--- app/src/main/java/com/example/sinfilas/Login.java
+++ app/src/main/java/com/example/sinfilas/Login.java
@@ -2,13 +2,53 @@ package com.example.sinfilas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
+    private EditText etusuario, etclave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etusuario = findViewById(R.id.etusuario);
+        etclave = findViewById(R.id.etclave);
+    }
+    // Ingresa y valida los datos de los usuarios registrados
+    public void validar (View view){
+        String usuario = etusuario.getText().toString();
+        String clave = etclave.getText().toString();
+        //  validacion de datos usuario y contraseña
+        if ( usuario.equals("admin") && clave.equals("12345")){
+            Intent i = new Intent(this, Login.class);
+            i.putExtra("Nombre",usuario);
+            startActivity(i);
+        }else{
+            Toast mensaje = Toast.makeText(this, "Por favor Verifique el usuario y contraseña",Toast.LENGTH_LONG);
+            mensaje.show();
+
+        }
+    }
+    // Ingresa a la activity de inicio
+    public void inicio (View view){
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+    }
+    // Ingresa a la activity mis citas
+    public void misCitas (View view){
+        Intent i = new Intent(this, Login.class);
+        startActivity(i);
+    }
+    // Ingresa a la activity regitro
+    public void nuevoregistro (View view){
+
+        Intent i = new Intent(this, Login.class);
+        startActivity(i);
+
     }
 }
--- app/src/main/java/com/example/sinfilas/MainActivity.java
+++ app/src/main/java/com/example/sinfilas/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.sinfilas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,4 +15,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     }
+
+    public void inicioSesion (View view){
+
+            Intent i = new Intent(this, Login.class);
+            //i.putExtra("Nombre",i);
+            startActivity(i);
+
+    }
+    public void Registro (View view){
+
+        Intent i = new Intent(this, Login.class);
+        startActivity(i);
+
+    }
+
+
 }
