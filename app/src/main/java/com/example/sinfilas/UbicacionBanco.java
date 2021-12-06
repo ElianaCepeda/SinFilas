22cffce0bcd0c545826931147d7ab7bddb88acf6
HECTOR EDUARDO ASTUDILLO <calvo2244@hotmail.com>
2021-12-06 17:46:08 -0500

    mapa google
    adicion mapa google

--- app/src/main/java/com/example/sinfilas/UbicacionBanco.java
+++ app/src/main/java/com/example/sinfilas/UbicacionBanco.java
@@ -1,55 +1,92 @@
 package com.example.sinfilas;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 
-public class UbicacionBanco extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class UbicacionBanco extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ubicacion_banco);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.mapView);
+        mapFragment.getMapAsync(this);
     }
 
     // Ir al activiy asignar cita
-    public void asignarcita (View view){
+    public void asignarcita(View view) {
         Intent i = new Intent(this, AsignarCita.class);
         startActivity(i);
     }
 
     @Override
-    public boolean onCreateOptionsMenu (Menu menu){
+    public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu1, menu);
         return true;
     }
-    public boolean  onOptionsItemSelected(MenuItem item){
-        int id =item.getItemId();
 
-        if (id== R.id.menu_perfil){
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.menu_perfil) {
             Intent i = new Intent(this, miperfil.class);
             startActivity(i);
 
-        }else if (id== R.id.menu_nuevacita){
+        } else if (id == R.id.menu_nuevacita) {
             Intent i = new Intent(this, UbicacionBanco.class);
             startActivity(i);
 
-        }else if (id==R.id.menu_miscitas) {
+        } else if (id == R.id.menu_miscitas) {
             Intent i = new Intent(this, miscitas.class);
             startActivity(i);
 
-        }else{
-            if (id==R.id.menu_cerrarsesion){
+        } else {
+            if (id == R.id.menu_cerrarsesion) {
                 Intent i = new Intent(this, MainActivity.class);
                 startActivity(i);
-                }
+            }
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+
+        // Add a marker in Colombia and move the camera
+        /*LatLng sydney = new LatLng(4.71193, -74.06731);
+        mMap.addMarker(new MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 10));*/
     }
 }
